bff97f254e4cc26421c8bc5f0090629f38e4c51a
Paweł Idzikowski <iddziku@gmail.com>
2020-09-21 13:12:17 +0200

    Add Spring Initializr configuration

---
+++ grocery-store-api/src/main/java/com/trolit/github/grocerystoreapi/GroceryStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.trolit.github.grocerystoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryStoreApiApplication.class, args);
+	}
+
+}
---
+++ grocery-store-api/src/test/java/com/trolit/github/grocerystoreapi/GroceryStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trolit.github.grocerystoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
