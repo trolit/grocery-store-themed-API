40de4674f12ce898a693fadb69631af19cd6ac43
Paweł Idzikowski <iddziku@gmail.com>
2020-09-24 00:18:02 +0200

    Migrate to Swagger v3

--- grocery-store/src/main/java/com/trolit/github/grocerystore/AppConfig.java
+++ grocery-store/src/main/java/com/trolit/github/grocerystore/AppConfig.java
@@ -1,5 +1,7 @@
 package com.trolit.github.grocerystore;
 
+import com.trolit.github.grocerystore.dto.product.ProductQueryDto;
+import com.trolit.github.grocerystore.models.Product;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +10,8 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class AppConfig {
 
     @Bean
