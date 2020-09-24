7a36d25d12262159b1538e34e30451c494e46682
Paweł Idzikowski <iddziku@gmail.com>
2020-09-24 10:23:00 +0200

    Update ProductCommandServiceImpl.java

--- grocery-store/src/main/java/com/trolit/github/grocerystore/services/product/ProductCommandServiceImpl.java
+++ grocery-store/src/main/java/com/trolit/github/grocerystore/services/product/ProductCommandServiceImpl.java
@@ -4,7 +4,9 @@ import com.trolit.github.grocerystore.dto.product.ProductCreateDto;
 import com.trolit.github.grocerystore.dto.product.ProductQueryDto;
 import com.trolit.github.grocerystore.dto.product.ProductUpdateDto;
 import com.trolit.github.grocerystore.models.Product;
+import com.trolit.github.grocerystore.repositories.CategoryRepository;
 import com.trolit.github.grocerystore.repositories.ProductRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,32 +14,39 @@ import org.springframework.stereotype.Service;
 public class ProductCommandServiceImpl implements  ProductCommandService {
 
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public ProductCommandServiceImpl(ProductRepository productRepository) {
+    public ProductCommandServiceImpl(ProductRepository productRepository,
+                                     CategoryRepository categoryRepository,
+                                     ModelMapper modelMapper) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
     public int createProduct(ProductCreateDto productCreateDto) {
-        Product product = new Product();
-        product.setName(productCreateDto.getName());
-        product.setPrice(productCreateDto.getPrice());
-        product.setStock(productCreateDto.getStock());
-        // check if that category exists!
-        // product.setCategory(new Category());
-        return productRepository.save(product).getId();
+        boolean isCategoryPresent = categoryRepository.findById(productCreateDto.getCategoryId()).isPresent();
+        if (isCategoryPresent) {
+            Product product = modelMapper.map(productCreateDto, Product.class);
+            product.setCategory(categoryRepository.findById(productCreateDto.getCategoryId()).get());
+            return productRepository.save(product).getId();
+        } else {
+            return 0;
+        }
     }
 
     @Override
     public ProductQueryDto updateProduct(int id, ProductUpdateDto productUpdateDto) {
-        if (productRepository.findById(id).isPresent()) {
-            Product product = productRepository.findById(id).get();
-            product.setName(productUpdateDto.getName());
-            product.setPrice(productUpdateDto.getPrice());
-            product.setStock(productUpdateDto.getStock());
+        boolean isProductPresent = productRepository.findById(id).isPresent();
+        boolean isCategoryPresent = categoryRepository.findById(productUpdateDto.getCategoryId()).isPresent();
+        if (isProductPresent && isCategoryPresent) {
+            Product product = modelMapper.map(productUpdateDto, Product.class);
+            product.setCategory(categoryRepository.findById(productUpdateDto.getCategoryId()).get());
             Product updatedProduct = productRepository.save(product);
-            return new ProductQueryDto(updatedProduct.getId(), updatedProduct.getName());
+            return modelMapper.map(updatedProduct, ProductQueryDto.class);
         } else {
             return null;
         }
